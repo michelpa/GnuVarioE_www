af6fe8e3fcea940bac30dfae44caff0a8d86e1cb
Alex <alex@MacBook-Pro-Alex.local>
2020-11-12 17:13:21 +0500

    add ru

--- dev/src/translation/index.js
+++ dev/src/translation/index.js
@@ -1,9 +1,11 @@
 import fr from './fr.json'
 import en from './en.json'
+import ru from './ru.json'
 
 export const defaultLocale = 'fr'
 
 export const languages = {
     fr: fr,
     en: en,
+    ru: ru,
 }
